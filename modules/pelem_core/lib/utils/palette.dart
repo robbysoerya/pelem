44a5c3bb2d48d6c38bd1edb9bbe2e0b73e7a9340
Robby Surya Pratama <robby@skinmystery.com>
2023-05-04 08:52:14 +0700

    refactor: [pelem_core] add color palette

---
+++ modules/pelem_core/lib/utils/palette.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class Palette {
+  static const Color primaryColor = Color(0xFF170447);
+  static const Color primaryAncentColor = Color(0xFFFBEAFF);
+  static const Color secondaryColor = Color(0xFF4A3078);
+}
